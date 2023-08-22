7ebd3d8774cb3688c5737f3f1c58475e2b634ee5
Francisco Perez-Reche <36167266+fjpreche@users.noreply.github.com>
2023-08-22 22:52:01 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ssSIG',
-    version='0.0.1',
+    version='1.0.0',
 
     url='https://github.com/MichaelKim0407/tutorial-pip-package',
     author='Francisco Perez-Reche',
