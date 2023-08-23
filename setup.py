8a1ff70babf264f943c5e670a71361cb8e5be606
Francisco Perez-Reche <36167266+fjpreche@users.noreply.github.com>
2023-08-23 19:58:54 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ssSIG',
-    version='1.0.0',
+    version='1.0.1',
 
     url='https://github.com/fjpreche/ssSIG.git',
     author='Francisco Perez-Reche',
